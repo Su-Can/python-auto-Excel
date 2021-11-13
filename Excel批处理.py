138b30683edc6b6d9097320421d2d4f664332473
Su-Can <94168532+Su-Can@users.noreply.github.com>
2021-11-13 18:03:06 +0800

    Add files via upload
    利用pandas模块对单一sheet页（工作簿）进行增加行，删除行，根据条件进行筛选的简单处理。

---
+++ Excel批处理.py
@@ -0,0 +1,23 @@
+# 引用 pandas
+import pandas as pd
+import numpy as np
+
+# 获取文件
+path = r"D:\wendang\python\test.xlsx"  # 文件路径
+# 打开表格
+data = pd.read_excel(path)  # 打开表格：如果是其他格式，需要改为read_XXX
+# 执行条件
+newData = data.drop([0, 1])  # 删除指定行：多行用列表"[]",axis默认为0
+newData_1 = newData.drop(['Unnamed: 10'], axis=1)  # 删除指定列：删除列需要指定axis=1
+# 删除列中含有特殊字符的行
+newData_1 = newData_1.astype(str)  # 转换字符类型为str
+newData_2 = newData_1[~newData_1['Unnamed: 9'].str.contains('（暂定）')]  # 反选'Unnamed: 9'列中，包含（暂定）的行，正向选择删掉”~“
+
+print(newData_2)
+# 加一个交互
+a = input("是否确认？\nY/是；N/否（请输入大写）\n")
+if str(a) == "Y":
+    newData_2.to_excel(r"D:\wendang\python\test_1.xlsx", index=False)  #保存
+    print(r'新建文件保存成功，路径为：D:\wendang\python\test_1.xlsx')
+else:
+    print("好的再见")
